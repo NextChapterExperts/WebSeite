398b4c6d4304f8c8446b8b11db33403580e8f282
Peter Alexander <kontakt@nextchapterexperts.de>
2025-05-16 09:32:12 +0200

    test

--- src/app/guide/page.tsx
+++ src/app/guide/page.tsx
@@ -2,7 +2,7 @@ export default function AboutPage() {
   return (
     <div className="container mx-auto py-16 text-center">
       <h1 className="text-4xl font-bold mb-6">Wie wir arbeiten</h1>
-      <p className="text-lg">Nachfolgende eine Beschreibung unserer Vorgehensweise</p>
+      <p className="text-lg">Nachfolgende eine Beschreibung unserer Vorgehensweise xxx</p>
     </div>
   );
 }
