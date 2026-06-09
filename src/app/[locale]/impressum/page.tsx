f70e54d4fad56dc52ebedece8ce6fac103155296
Peter Alexander <peter.alexander@nextchapterexperts.de>
2026-06-09 09:53:00 +0000

    fix: Link-Import → @/i18n/navigation (locale-aware) für Datenschutz-Link

--- src/app/[locale]/impressum/page.tsx
+++ src/app/[locale]/impressum/page.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import { Link } from "@/i18n/navigation";
 import { getTranslations } from "next-intl/server";
 
 type Props = { params: Promise<{ locale: string }> };
